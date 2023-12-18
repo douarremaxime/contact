a24620d483000376e31b9fe720da50f1bdd65b01
Maxime <douarremaxime@gmail.com>
2023-12-18 23:48:21 +0100

    Init middlewares

--- Contact/Program.cs
+++ Contact/Program.cs
@@ -1,6 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler();
+    app.UseStatusCodePages();
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+var options = new DefaultFilesOptions();
+options.DefaultFileNames[0] = "signin.html";
+app.UseDefaultFiles(options);
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
