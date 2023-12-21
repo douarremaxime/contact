29eda5eaaa2467fdd4248890ecb901eca29d5dc3
Maxime <douarremaxime@gmail.com>
2023-12-21 22:13:51 +0100

    Add IdentityController.cs

---
+++ Contact/Controllers/IdentityController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.Controllers
+{
+    /// <summary>
+    /// Identity controller.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class IdentityController : ControllerBase
+    {
+    }
+}
