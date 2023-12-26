dbe4c94c81d46ef20ce1eac96335ff41e4568148
Maxime <douarremaxime@gmail.com>
2023-12-26 22:14:48 +0100

    Add IdentityController.RegisterAsync()

--- Contact/Controllers/IdentityController.cs
+++ Contact/Controllers/IdentityController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Contact.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Contact.Controllers
 {
@@ -9,5 +13,46 @@ namespace Contact.Controllers
     [Route("[controller]")]
     public class IdentityController : ControllerBase
     {
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <param name="request">Registration request.</param>
+        /// <param name="userManager">User manager.</param>
+        /// <returns>No content.</returns>
+        [HttpPost("register")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [AllowAnonymous]
+        public async Task<ActionResult> RegisterAsync(
+            RegisterRequest request,
+            UserManager<IdentityUser<long>> userManager)
+        {
+            var user = new IdentityUser<long>(request.Username);
+
+            var result = await userManager.CreateAsync(
+                user,
+                request.Password);
+
+            if (!result.Succeeded)
+            {
+                return CreateValidationProblem(result);
+            }
+
+            return NoContent();
+        }
+
+        private ActionResult CreateValidationProblem(IdentityResult result)
+        {
+            var errorDictionary = new ModelStateDictionary();
+
+            foreach (var error in result.Errors)
+            {
+                errorDictionary.AddModelError(
+                    error.Code,
+                    error.Description);
+            }
+
+            return ValidationProblem(errorDictionary);
+        }
     }
 }
