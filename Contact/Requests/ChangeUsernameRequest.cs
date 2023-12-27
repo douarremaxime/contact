739a75d76cfeaadaae0484c033506c04053f3b9d
Maxime <douarremaxime@gmail.com>
2023-12-27 21:06:25 +0100

    Add ChangeUsernameRequest.cs

---
+++ Contact/Requests/ChangeUsernameRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contact.Requests
+{
+    /// <summary>
+    /// Request to change a username.
+    /// </summary>
+    public record class ChangeUsernameRequest
+    {
+        /// <summary>
+        /// New username.
+        /// </summary>
+        [Required]
+        public required string NewUsername { get; init; }
+    }
+}
