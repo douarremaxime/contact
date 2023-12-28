16c4a384ce1383d0e3334c9e4e0a7947ea5b15e3
Maxime <douarremaxime@gmail.com>
2023-12-28 01:04:42 +0100

    Add ChangePasswordRequest.cs

---
+++ Contact/Requests/ChangePasswordRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contact.Requests
+{
+    /// <summary>
+    /// Request to change a password.
+    /// </summary>
+    public record class ChangePasswordRequest
+    {
+        /// <summary>
+        /// New password.
+        /// </summary>
+        [Required]
+        public required string NewPassword { get; init; }
+    }
+}
