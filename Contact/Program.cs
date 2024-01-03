29e17ece90c16e2067afde263e1ada521e851323
Maxime <douarremaxime@gmail.com>
2024-01-03 23:44:50 +0100

    Configure swagger

--- Contact/Program.cs
+++ Contact/Program.cs
@@ -1,8 +1,14 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 builder.Services.AddProblemDetails();
 
@@ -15,23 +21,28 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 else
 {
     app.UseExceptionHandler();
     app.UseStatusCodePages();
+
     app.UseHsts();
+    app.UseHttpsRedirection();
+
+    var options = new DefaultFilesOptions();
+    options.DefaultFileNames[0] = "signin.html";
+    app.UseDefaultFiles(options);
+
+    app.UseStaticFiles();
 }
-
-app.UseHttpsRedirection();
-
-var options = new DefaultFilesOptions();
-options.DefaultFileNames[0] = "signin.html";
-app.UseDefaultFiles(options);
-
-app.UseStaticFiles();
 
 app.UseRouting();
 
