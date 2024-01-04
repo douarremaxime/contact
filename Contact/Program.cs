b93db67209838f72b162c6e59ad9e1096f38d238
Maxime <douarremaxime@gmail.com>
2024-01-04 20:10:06 +0100

    Restore UseStatusCodePages middleware

--- Contact/Program.cs
+++ Contact/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddProblemDetails();
 
 builder.Services.AddContactIdentity();
@@ -11,6 +10,7 @@ builder.Services.AddContactStores(builder.Configuration);
 var app = builder.Build();
 
 app.UseExceptionHandler();
+app.UseStatusCodePages();
 
 if (!app.Environment.IsDevelopment())
     app.UseHsts();
