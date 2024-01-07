fd5cb3fa79274d96b4138c01176244f9f92110b0
Maxime <douarremaxime@gmail.com>
2024-01-07 20:05:49 +0100

    Handle fetch errors in signin.js

--- Contact/wwwroot/scripts/signin.js
+++ Contact/wwwroot/scripts/signin.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("form");
+
 const errorsWrapper = document.querySelector("#errors");
+const errorsList = errorsWrapper.querySelector("ul");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -11,6 +13,13 @@ form.addEventListener("submit", async (event) => {
     if (response.ok) {
       window.location = "/";
     } else {
+      const result = await response.json();
+      const errors = Object.values(result.errors).map((error) => {
+        const li = document.createElement("li");
+        li.textContent = error[0];
+        return li;
+      });
+      errorsList.replaceChildren(...errors);
       errorsWrapper.removeAttribute("hidden");
     }
   } catch (e) {
