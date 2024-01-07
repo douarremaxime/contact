6f0fd9a100125ef781c10087345872e168b87982
Maxime <douarremaxime@gmail.com>
2024-01-07 18:55:59 +0100

    Update fetch in signup.js

--- Contact/wwwroot/scripts/signup.js
+++ Contact/wwwroot/scripts/signup.js
@@ -21,22 +21,17 @@ form.addEventListener("submit", async (event) => {
       method: "POST",
       body: new URLSearchParams(new FormData(form)),
     });
-    switch (response.status) {
-      case 204:
-        window.location = "/signup-successful.html";
-        break;
-      case 400:
-        const result = await response.json();
-        const errors = Object.values(result.errors).map((error) => {
-          const li = document.createElement("li");
-          li.textContent = error[0];
-          return li;
-        });
-        errorsList.replaceChildren(...errors);
-        errorsWrapper.removeAttribute("hidden");
-        break;
-      default:
-        throw new Error(`Unexpected status code: ${response.status}`);
+    if (response.ok) {
+      window.location = "/signup-successful.html";
+    } else {
+      const result = await response.json();
+      const errors = Object.values(result.errors).map((error) => {
+        const li = document.createElement("li");
+        li.textContent = error[0];
+        return li;
+      });
+      errorsList.replaceChildren(...errors);
+      errorsWrapper.removeAttribute("hidden");
     }
   } catch (e) {
     console.error(e);
