34a63732bd0272940b2e8c56a6ad3fc1a78cb3d1
Maxime <douarremaxime@gmail.com>
2024-01-10 19:43:51 +0100

    Null username exception

---
+++ Contact/Exceptions/NullUsernameException.cs
@@ -0,0 +1,27 @@
+﻿namespace Contact.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when a user does not have a username.
+    /// </summary>
+    public class NullUsernameException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="NullUsernameException"/>.
+        /// </summary>
+        public NullUsernameException()
+        {
+        }
+
+        /// <inheritdoc/>
+        public NullUsernameException(string message)
+            : base(message)
+        {
+        }
+
+        /// <inheritdoc/>
+        public NullUsernameException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
