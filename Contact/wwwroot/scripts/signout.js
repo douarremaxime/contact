59287da0086cb52059451bea5694279e16619eee
Maxime <douarremaxime@gmail.com>
2024-01-16 22:45:40 +0100

    Prepare user-settings.html for <dialog>

--- Contact/wwwroot/scripts/signout.js
+++ Contact/wwwroot/scripts/signout.js
@@ -1,5 +1,4 @@
 const form = document.querySelector("header form");
-const errorWrapper = form.nextElementSibling;
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -13,9 +12,6 @@ form.addEventListener("submit", async (event) => {
     if (response.ok) {
       window.location = "/signin.html";
     } else {
-      const result = await response.json();
-      errorWrapper.textContent = result.title;
-      errorWrapper.removeAttribute("hidden");
     }
   } catch (e) {
     console.error(e);
